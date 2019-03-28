2ddf3705bba3c119971141ba4900a94b6190feac
satyajitdey02 <satyajit02@gmail.com>
2019-03-28 12:27:45 +0600

    Update Junit test case in r6-* branch

---
+++ src/test/java/com/cefalo/workshop/SshClientTests.java
@@ -0,0 +1,37 @@
+package com.cefalo.workshop;
+
+import com.cefalo.workshop.cli.CLI;
+import com.cefalo.workshop.domain.ServerInstanceManager;
+import com.jcraft.jsch.JSchException;
+import java.io.IOException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by satyajit on 3/28/19.
+ */
+public class SshClientTests {
+
+  private CLI cli;
+
+  private static final String SERVER_INSTANCE = "app2";
+  private static final String COMMAND = "whoami";
+
+  @Before
+  public void before() {
+    ServerInstanceManager instanceManager = new ServerInstanceManager();
+    cli = instanceManager.connectToServer(SERVER_INSTANCE);
+  }
+
+  @Test
+  public void testSshConnection() throws IOException, JSchException {
+    cli.execute(COMMAND);
+  }
+
+  @After
+  public void after() {
+    cli.exit();
+  }
+
+}
